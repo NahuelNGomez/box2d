816b12d49e7446b17e8143fd1018f889f2508556
Gomez Nahuel <nngomez@fi.uba.ar>
2022-10-28 13:44:29 -0300

    Añado archivos correspondientes a la librería Box2D

---
+++ extern/sajson/CMakeFiles/sajson.dir/sajson.cpp.o.d
@@ -0,0 +1,3 @@
+box2d/extern/sajson/CMakeFiles/sajson.dir/sajson.cpp.o: \
+ /home/taller/Documentos/Taller/a/Taller-1-Rocket-League/testingBox2D/box2d/extern/sajson/sajson.cpp \
+ /usr/include/stdc-predef.h
